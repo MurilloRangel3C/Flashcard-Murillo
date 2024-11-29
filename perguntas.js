2a359080bb1153285e20ab04ff49e34dc5f8d0a7
MurilloRangel3C <00001105149365sp@al.educacao.sp.gov.br>
2024-11-29 15:39:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação'
+)
+criaCartao(
+    'Biologia',
+    'O que é DNA?',
+    'ácido desoxirribonucleico, uma molécula que contém o material genético de todos os seres vivos, exceto alguns vírus'
+)
+criaCartao(
+    'Filosofia',
+    'Qual o período de Platão?',
+    'Período da Grécia antiga'
+)
+
+criaCartao(
+    'Inglês',
+    'Qual a tradução de Lend',
+    'A tradução de Lend é emprestar'
+)
